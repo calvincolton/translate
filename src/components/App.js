55841e68372162bd96a0d14579b33bf3f28648ee
Calvin Colton <calvincolton@gmail.com>
2019-01-29 20:42:37 -0800

    boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import UserCreate from './UserCreate';
+
+class App extends Component {
+  state = {
+    language: 'english'
+  };
+
+  onLanguageChange = (language) => {
+    this.setState({ language })
+  };
+
+  render() {
+    return (
+      <div className="app ui container">
+        <div>
+          Select a language:
+          <i onClick={() => this.onLanguageChange('english')} className="flag us" />
+          <i onClick={() => this.onLanguageChange('dutch')} className="flag nl" />
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
+
+export default App;
